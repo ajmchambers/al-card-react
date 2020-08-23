1a0edbe4c0da6dcac2013ea3b7dc31c74d87ccc3
Alan Chambers <ajmchambers@gmail.com>
2020-08-23 05:24:16 +0100

    first commit

---
+++ dist/components.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+export const AlCard = /*@__PURE__*/ createReactComponent('al-card');
+//# sourceMappingURL=components.js.map
---
+++ src/components.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@ajmchambers/al-card';
+
+
+
+export const AlCard = /*@__PURE__*/createReactComponent<JSX.AlCard, HTMLAlCardElement>('al-card');
